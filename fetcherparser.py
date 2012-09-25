2f0e2b2a6ec5438f9b05a49e28310bedc7ed5ede
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-09-25 10:45:32 +0300

    stub

---
+++ fetcherparser.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""This module will handle fetching the JSON and parsing it to do recursive fetches
+of messages and their resources (images etc)"""
+import json, urllib2
