5f52d629efacadf96bd9ab8eb8729583a246bafc
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-09-25 10:47:13 +0300

    stubs

---
+++ channeldump.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""This tool will fetch a channel stream and dump it using the fetchparser"""
+import fetchparser
---
+++ userdump.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""This tool will fetch users Qaiku stream and dump it using the fetchparser"""
+import fetchparser
