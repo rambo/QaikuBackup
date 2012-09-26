3bd06fed873b4626d86ecd1e8fb769584d570125
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-09-26 18:51:15 +0300

    returning the list as received for now, will think about this later...

--- channeldump.py
+++ channeldump.py
@@ -7,6 +7,7 @@ def fetch_channel_messages(channel):
     """Messages in the channel (and comments etc because this all goes to the recursive fetching system)"""
     messages = fetcherparser.fetch_paged("http://www.qaiku.com/api/statuses/channel_timeline/%s.json" % channel)
     fetcherparser.mass_insert_and_recurse(messages)
+    return messages
 
 if __name__ == '__main__':
     import sys,os,json
